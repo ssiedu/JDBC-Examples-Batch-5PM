4929a3e69fea5a27267e7604ae33c2d488479f89
manoj <manojs121@rediffmail.com>
2021-07-10 17:58:36 +0530

    100721

---
+++ src/DataEntry.java
@@ -0,0 +1,38 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DataEntry {
+
+	public static void main(String[] args) {
+		//this code will enter a new record in emp table
+		
+		try {
+			//step-1 (Driver Loading)
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver Loaded Successfully...!");
+			
+			//step-2 (Connection Establishment)
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/data5","root","root");
+			System.out.println("Connected Successfully...!");
+			
+			//step-3 (Define A SQL Command)
+			String sql="INSERT INTO emp VALUES(112,'BBB',20000)";
+			
+			//step-4 (Dispatching SQL Command To DB using Statement Object)
+			Statement stmt=con.createStatement();
+			int n=stmt.executeUpdate(sql);
+			System.out.println(n+" row added...!");
+			
+			//step-5 (Close The Connection)
+			con.close();
+			
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
